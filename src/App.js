53847849bd0c122049b0e9abd11c7e9cdf4bcbf9
mukeshj2110 <mukeshjadhav680@gmail.com>
2021-05-10 23:45:46 +0530

    Added Header & NavBar Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Header from './components/Header'
+import MainNav from './components/MainNav'
+import {BrowserRouter} from 'react-router-dom'
+
 
 function App() {
   return (
+    <BrowserRouter>
+     <Header />
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
+    <MainNav />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <span className="title"> 🎥 Movies & TV Series Hub🎬</span>
+        </div>
+    )
+}
+
+export default Header
