6826f1bc6ffc46d75703a1fc66efaaf064da3dd0
mukeshj2110 <mukeshjadhav680@gmail.com>
2021-05-12 17:06:38 +0530

    added contentModal component

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -3,9 +3,10 @@ import '../../Config/config'
 import './singlecontent.css'
 import { img_300, unavailable } from '../../Config/config';
 import { Badge } from '@material-ui/core';
+import ContentModal from '../ContentModal/ContentModal';
 const SingleContent = ({poster,title , id, date, media_type,vote_average}) => {
     return (
-        <div className="media">
+        <ContentModal media_type={media_type} id={id} poster={poster}>
             <Badge badgeContent={vote_average} color={ vote_average>6 ? 'primary' : 'secondary' }/>
             <img className="poster" src={poster? `${img_300}/${poster}`: unavailable}  alt='{title}'/>
             <b className="title">{title}</b>
@@ -14,7 +15,7 @@ const SingleContent = ({poster,title , id, date, media_type,vote_average}) => {
             <span className="subTitle">{date}</span>
             </span>
             
-        </div>
+        </ContentModal>
     )
 }
 
